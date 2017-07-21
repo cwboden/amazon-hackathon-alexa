651d971264adf19be5d57f2585aef6b6822fd796
Twitchell <ctwitch@amazon.com>
2017-07-21 19:29:54 +0100

    Add in logic for recipe steps and ingredients

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ var APP_ID = 'amzn1.ask.skill.85ae7ea2-b727-4d2a-9765-5c563a5ec379';
 var SKILL_NAME = 'Snack Overflow';
 var POSSIBLE_RECIPES = ['Chicken Parmesan', 'Spaghetti', 'Turkey Sandwich'];
 
-var currentRecipe, currentRecipeName;
+var currentRecipe, currentRecipeName, currentStep = 0;
 
 var WORKFLOW_STATES = {
     START : 1,
-    RECIPE_PROPOSED : 2
+    RECIPE_PROPOSED: 2,
+    INGREDIENTS_PROPOSED: 3
 };
 
 var currentWorkflowState = WORKFLOW_STATES.START;
@@ -33,14 +34,19 @@ var utilHandlers = {
     },
     'negativeIntent': function () {
         doNegativeAction();
+    },
+    'nextStepIntent': function () {
+        tellStep(currentRecipe, ++currentStep);
+    },
+    'repeatStepIntent': function () {
+        tellStep(currentRecipe, currentStep);
     }
 };
 
 function suggestRecipe() {
-    var recipeNum = Math.floor(Math.random() * POSSIBLE_RECIPES.length);
-    currentRecipeName = POSSIBLE_RECIPES[recipeNum];
+    var currentRecipe = getRandomRecipe();
 
-    this.emit(':tell', "Would you like to make ", currentRecipeName);
+    this.emit(':tell', "Would you like to make ", currentRecipe.recipes.title);
     currentWorkflowState = WORKFLOW_STATES.RECIPE_PROPOSED;
 }
 
@@ -49,8 +55,14 @@ function doAffirmativeAction() {
         case (WORKFLOW_STATES.START):
             break;
         case (WORKFLOW_STATES.RECIPE_PROPOSED):
-            currentRecipe = getRecipeFromAPI(currentRecipeName);
-            tellRecipe(currentRecipe);
+            // user said they wanted to do the proposed recipe. ask if they have ingredients
+            currentRecipe = getRandomRecipe(currentRecipeName);
+            tellIngredients(currentRecipe);
+            currentWorkflowState = WORKFLOW_STATES.INGREDIENTS_PROPOSED;
+            break;
+        case (WORKFLOW_STATES.INGREDIENTS_PROPOSED):
+            // user said they have the ingredients. begin listing steps to recipe
+            tellStep(currentRecipe, currentStep);
             break;
     }
 }
@@ -62,18 +74,25 @@ function doNegativeAction() {
         case (WORKFLOW_STATES.RECIPE_PROPOSED):
             suggestRecipe();
             break;
+        case (WORKFLOW_STATES.INGREDIENTS_PROPOSED):
+            // TODO: insert 'would you like to buy ingredients?' logic
+            suggestRecipe();
+            break;
     }
 }
 
 function tellRecipe(recipe) {
-    /*
     this.emit(":tell", "You will need");
-    for (var i = 0; i < recipe.ingredients.length; i++) {
-        this.emit(":tell", recipe.ingredients[i].quantity, "of", recipe.ingredients[i].name);
+
+    for (var i = 0; i < recipe.extendedIngredients.length; i++) {
+        this.emit(":tell", recipe.extendedIngredients[i].originalString);
     }
 
-    for (var i = 0; i < recipe.steps.length; i++) {
-        this.emit)(":tell", recipe.steps[i]);
-    }
-    */
+    this.emit(":tell", "Do you have these ingredients?");
+}
+
+function tellStep(recipe, step) {
+    // TODO: insert logic for no more steps
+    this.emit(":tell", recipe.instructions[step]);
+    this.emit(":tell", "Tell me when you're ready for the next step, or if you need to hear that step again.");
 }
