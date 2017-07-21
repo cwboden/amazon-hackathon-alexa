84df72679abd7276f6f207443681f2505a093fd0
Francis <jxf@amazon.com>
2017-07-21 12:17:10 -0700

    fixed handlers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 var Alexa = require('alexa-sdk');
 
-var handlers = [require('./Handlers/getFridgeHandlers.js'),
-                require('./Handlers/getRecipeHandlers.js'),
-                require('./Handlers/utilHandlers.js')]
-
 var APP_ID = 'amzn1.ask.skill.85ae7ea2-b727-4d2a-9765-5c563a5ec379';
 
 var SKILL_NAME = 'Snack Overflow';
@@ -20,7 +16,10 @@ var currentWorkflowState = WORKFLOW_STATES.START;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context, callback);
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(
+            require('./Handlers/getFridgeHandlers.js'),
+            require('./Handlers/getRecipeHandlers.js'),
+            require('./Handlers/utilHandlers.js'));
     alexa.execute();
 };
 
