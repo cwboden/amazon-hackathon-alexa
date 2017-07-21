baf1ab741408d5040ebbae08ea2091d17fe6ec2f
Twitchell <ctwitch@amazon.com>
2017-07-21 21:55:16 +0100

    Add start state

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ var APP_ID = 'amzn1.ask.skill.85ae7ea2-b727-4d2a-9765-5c563a5ec379';
 var SKILL_NAME = 'Snack Overflow';
 var POSSIBLE_RECIPES = ['Chicken Parmesan', 'Spaghetti', 'Turkey Sandwich'];
 
-var currentWorkflowState = STATES.START;
-
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context, callback);
+    alexa.state = STATES.START;
     alexa.registerHandlers(
         require('./handlers/StartHandlers'),
         require('./handlers/FridgeHandlers'),
